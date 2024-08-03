1f305788a970ff261a6b194cc7c39f983ce25b45
gbonavina <gabriellbonavina@gmail.com>
2024-08-03 17:25:24 -0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -16,7 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 url = 'https://investidor10.com.br/fiis/rankings/maior-valor-patrimonial/'
 
 # Setup do ChromeDriver
-service = Service(ChromeDriverManager(version="114.0.5735.90").install())
+service = Service(ChromeDriverManager().install(version="114.0.5735.90"))
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # Run in headless mode for CI environment
 options.add_argument("--no-sandbox")  # Bypass OS security model
