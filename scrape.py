0efdafc35d7fdc4a1e3b5829f24304fa02075432
gbonavina <gabriellbonavina@gmail.com>
2024-09-23 14:59:50 -0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
